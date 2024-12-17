b4979c538eb8f3ac1329099e9093be528ad6003c
rupam-mahanta <rupammahanta73@gmail.com>
2024-12-17 19:12:41 +0530

    initial first commit

---
+++ program1-maths.py
@@ -0,0 +1,9 @@
+# Taking two numbers as input from the user
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+# Adding the two numbers
+result = number1 + number2
+
+# Displaying the result
+print("The sum of", number1, "and", number2, "is:", result)
